39329964af69a8fb9b7de1f7749265704d6d0fa7
Parminder <parminder.singh.coder@gmail.com>
2024-09-29 01:43:46 +1200

    New Practice test on map and filter function

---
+++ Give Sibling Elements a Unique Key Attribute.js
@@ -0,0 +1,24 @@
+const frontEndFrameworks = [
+    'React',
+    'Angular',
+    'Ember',
+    'Knockout',
+    'Backbone',
+    'Vue'
+  ];
+  
+  function Frameworks() {
+    const renderFrameworks = frontEndFrameworks.map(
+      (frame, index) => (
+        <li key={index}>{frame}</li>
+      )
+    ); // Change this line
+    return (
+      <div>
+        <h1>Popular Front End JavaScript Frameworks</h1>
+        <ul>
+          {renderFrameworks}
+        </ul>
+      </div>
+    );
+  };
